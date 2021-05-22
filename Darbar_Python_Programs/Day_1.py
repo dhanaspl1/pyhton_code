6bc559d68b2a7ccce711ab00c8ef96fe26a30b36
dhanaspl1 <darbarkiet@gmail.com>
2021-05-22 22:51:19 +0530

    Day1
    if-elif-else-while loops

---
+++ Darbar_Python_Programs/Day_1.py
@@ -0,0 +1,24 @@
+if 'hello' == "hello" :
+	print('same')
+
+i = 1
+
+if i == 1:
+	print('true')
+	if i==0 :
+		print()
+	elif i==1:
+		print("elif completed")
+		if i==0 :
+			print()
+		elif i==3:
+			print()
+		else:
+			print('if elif else completed')
+
+while i<10:
+	i = i*1
+	i=i+1
+	print(i)
+
+print("basic things done, tmrw will go with datatypes and rest")
