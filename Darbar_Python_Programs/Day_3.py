a2478f0e8a5f8dcb2479370d657b1f4883bf727e
dhanaspl1 <darbarkiet@gmail.com>
2021-05-24 23:40:21 +0530

    Day3
    Reversing a string using slice

---
+++ Darbar_Python_Programs/Day_3.py
@@ -0,0 +1,25 @@
+''' Write a program to reverse an array or string
+Difficulty Level : Basic
+Last Updated : 08 Sep, 2020
+ 
+Given an array (or string), the task is to reverse the array/string.
+Examples : 
+ 
+
+Input  : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4} '''
+
+
+a = [1, 2, 3]
+
+b = [4, 5, 1, 2]
+
+def reverseList(A):
+  print( A[::-1])
+
+reverseList(a)
+
+reverseList(b)
