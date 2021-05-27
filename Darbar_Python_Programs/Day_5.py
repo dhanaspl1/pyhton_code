df13901f935c6b0d4be4cb112882977860a55809
dhanaspl1 <darbarkiet@gmail.com>
2021-05-27 23:02:00 +0530

    Day_6 done but still need to work on this

--- Darbar_Python_Programs/Day_5.py
+++ Darbar_Python_Programs/Day_5.py
@@ -1,13 +1,22 @@
-t = int(input());
+t = int(input())
 for i in range(t):
     b = int(input())
-	arr = str(input()).split(' ')
-	sum = 0
-	for n in range(b):
-	    sum += (int(arr[n])*int(arr[n]) - int(arr[n+1])*int(arr[n+1]))
-	    n=n+1
-	    if n == (b-1):
-	       break
-	print(sum)
+    arr = str(input()).split(' ')
+    a = 0
+    d = 0
+    sum = 0
+    if b == 2 :
+        sum = (int(arr[0])*int(arr[0]))-(int(arr[1])*int(arr[1]))
+        print(abs(sum))
+    else :
+        for n in range(b):
+            if n%2 == 0 :
+                a += int(arr[n])*int(arr[n]) 
+            else :
+                d += int(arr[n])*int((arr[n]))
+            if n == (b-1):
+                break
+        sum = a - d
+        print(sum)
     if t == 1:
         break
